894b6072a3ce4a1336de305ecc8e3bb99abffee6
Samuel Zheng <samuel.zheng1357@gmail.com>
2024-07-18 10:40:36 +1000

    initial commit

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        ensure_intalled = { "all" },
+        indent = { enable = true },
+      })
+    end
+  }
+}
